59bc4f46e007b931402dd3cdafc55a6b4f21949b
Dmitry Badichan <badimalex@gmail.com>
2020-04-28 10:28:21 +0200

    Add apollo and simple data fetching

--- react-front/app/containers/HomePage/index.js
+++ react-front/app/containers/HomePage/index.js
@@ -6,13 +6,38 @@
  */
 
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
+const EXCHANGE_RATES = gql`
+  {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
 export default function HomePage() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <div>
+      <h1>
+        <FormattedMessage {...messages.header} />
+      </h1>
+      {data.rates.map(({ currency, rate }) => (
+        <div key={currency}>
+          <p>
+            {currency}: {rate}
+          </p>
+        </div>
+      ))}
+    </div>
   );
 }
