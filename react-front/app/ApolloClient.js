2525ac6b4da10671001e2a1b65577379fdf33379
Dmitry Badichan <badimalex@gmail.com>
2020-05-19 21:32:45 +0200

    [system] fix path

--- react-front/app/ApolloClient.js
+++ react-front/app/ApolloClient.js
@@ -1,7 +1,10 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri:
+    process.env.NODE_ENV === 'production'
+      ? 'https://automation-consulting-services.herokuapp.com/graphql'
+      : 'http://localhost:3000/graphql',
 });
 
 export default client;
